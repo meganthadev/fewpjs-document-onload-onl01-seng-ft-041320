de08407bac20b1250b708d127aaa322aff38ab06
Megan Horsfield <meganthadev@gmail.com>
2020-07-21 23:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     updateDOM()
 });
+
+function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
